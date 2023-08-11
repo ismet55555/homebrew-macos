aba30cb966c31141d9392dfb1eceee3842ffc549
Ismet Handzic <ismet55555@users.noreply.github.com>
2023-08-11 09:25:08 -0400

    Update bieye.rb

--- Formula/bieye.rb
+++ Formula/bieye.rb
@@ -2,8 +2,8 @@ class Bieye < Formula
   desc "Read text in bionic reading format"
   homepage "https://github.com/ismet55555/bieye"
   license "GPL-3.0-or-later"
-  url "https://github.com/ismet55555/bieye/archive/refs/tags/v0.2.0.tar.gz"
-  sha256 "69403333dc1c35f2ca08d74de416079393545062b7e34fee0c1695a2e3800790"
+  url "https://github.com/ismet55555/bieye/archive/refs/tags/v0.2.1.tar.gz"
+  sha256 "a57b78213918b47d4872aa3462260247014562ce431b3a983a4e93ab2410388e"
 
   depends_on "rust" => :build
 
